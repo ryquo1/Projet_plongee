933b038af3d7272aa25a8ff211cd0dd6002a383a
Arthur <adelcourt40@gmail.com>
2024-03-23 19:31:32 +0100

    mise aux normes de l'affichage

--- app/src/main/java/com/example/projet_plongee/ViewInterface.kt
+++ app/src/main/java/com/example/projet_plongee/ViewInterface.kt
@@ -11,7 +11,7 @@ class ViewInterface : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_view_interface)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainView)) { v, insets ->
             val systemBars =
                 insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(
